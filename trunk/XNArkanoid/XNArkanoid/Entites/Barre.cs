5c6aae11f24fc9cd776cee6b229a089582b51f25
olivier.charton.pro@gmail.com <olivier.charton.pro@gmail.com@f26e9e05-36a5-e5f1-856b-cbf822667090>
2014-12-11 16:46:25 +0000

    New entities : classes ball, brick & Barre (TODO : change name)

---
+++ trunk/XNArkanoid/XNArkanoid/Entites/Barre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace XNArkanoid.Entites
+{
+    class Barre
+    {
+        public Rectangle Rectangle;
+        public int Speed { get; set; }
+        public int Width
+        {
+            get;
+            set
+            {
+                this.Width = value;
+                this.Rectangle.Width = value;
+            }
+        }
+
+        public Barre(Rectangle rectangle, int speed, int width)
+        {
+            this.Rectangle = rectangle;
+            this.Speed = speed;
+            this.Width = width;
+        }
+    }
+}
---
+++ trunk/XNArkanoid/XNArkanoid/Entites/Brick.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace XNArkanoid.Entites
+{
+    class Brick
+    {
+        public Rectangle Rectangle;
+        public int Width { get; set; }
+
+        public Brick(Rectangle rectangle, int Width)
+        {
+            this.Rectangle = rectangle;
+            this.Width = Width;
+        }
+
+    }
+}
