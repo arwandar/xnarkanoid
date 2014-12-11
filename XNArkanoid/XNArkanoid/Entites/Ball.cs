4f8227ae13a17d7bbcb81e2c492daf97fc85acec
olivier.charton.pro@gmail.com <olivier.charton.pro@gmail.com@f26e9e05-36a5-e5f1-856b-cbf822667090>
2014-12-11 16:46:25 +0000

    New entities : classes ball, brick & Barre (TODO : change name)

---
+++ XNArkanoid/XNArkanoid/Entites/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace XNArkanoid.Entites
+{
+    class Ball
+    {
+        public Rectangle Rectangle;
+        public float Speed { get; set; }
+        //public Vector2 spriteSpeed;
+        public Vector2 BallDirection;
+
+
+        public Ball(Rectangle rectangle)
+        {
+            this.Rectangle = rectangle;
+        }
+
+
+        public bool Collision (Barre barre)
+        {
+            if (barre.Rectangle.Intersects(this.Rectangle))
+            {
+                this.BallDirection.Y = -this.BallDirection.Y;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Collision(Brick brick)
+        {
+            if (brick.Rectangle.Intersects(this.Rectangle))
+            {
+                this.BallDirection.Y = -this.BallDirection.Y;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
