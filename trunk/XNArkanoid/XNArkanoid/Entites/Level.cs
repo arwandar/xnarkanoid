86e139760f906255791c0a3985f163f7e35e3ec9
olivier.charton.pro@gmail.com <olivier.charton.pro@gmail.com@f26e9e05-36a5-e5f1-856b-cbf822667090>
2014-12-18 16:04:28 +0000

    Début de Level.cs

---
+++ trunk/XNArkanoid/XNArkanoid/Entites/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace XNArkanoid.Entites
+{
+    class Level
+    {
+        public Barre Barre { get; set; }
+        public Ball Ball { get; set; }
+        public Brick[,] Bricks { get; set; }
+    }
+}
